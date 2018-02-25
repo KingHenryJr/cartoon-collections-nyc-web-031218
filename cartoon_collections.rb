785c74963e853cab98e625acb5cb23a6687ed18a
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-25 17:50:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,9 +22,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(foods)
-
- cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
-    cheese_types.include?(cheese)
-  end
+cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
